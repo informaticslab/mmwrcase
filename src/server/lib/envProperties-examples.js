ce522ac0ab5dd7ccb8988878168a64a7a489bb2e
Michael Ta <technical.ta@gmail.com>
2015-10-22 09:31:07 -0400

    re-add envProperties example file

---
+++ src/server/lib/envProperties-examples.js
@@ -0,0 +1,11 @@
+//create new file named 'envProperties.js' with the correct local credentials for development
+
+
+module.exports = {
+	
+	mysqlhost: '[mysql-host]',
+	mysqluser: '[mysql-user]',
+	mysqlpassword: '[mysql-password]',
+	mysqldatabase: '[mysql-database]'
+
+};
