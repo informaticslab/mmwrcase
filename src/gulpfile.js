56292152b288f9d60cfb8cc79af3c534b0ad18ae
Michael Ta <technical.ta@gmail.com>
2015-12-17 15:39:55 -0500

    updated gulp file

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,10 +1,16 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
 	sass = require('gulp-ruby-sass'),
 	minifycss = require('gulp-minify-css'),
 	uglify = require('gulp-uglify'),
+	autoprefixer = require('gulp-autoprefixer'),
 	livereload = require('gulp-livereload');
 
 gulp.task('styles', function() {
-	return gutil.log('Gulp is running!');
+	return sass('public/scss/*.scss', {style:'expanded'})
+	.pipe(autoprefixer('last 2 versions'))
+	.pipe(gulp.dest('public/css'))
+	.pipe(rename({suffix:'.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('public/css'))
+	.pipe(notify({message: 'Styles task complete'}));
 });
