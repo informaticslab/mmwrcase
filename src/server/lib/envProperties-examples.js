db2cce065aefebb548b82e31326d719541bfa813
Peter White <pwhitebe@gmail.com>
2016-09-28 14:46:41 -0400

    Committing updates to the example properties file that will make for better setup.

--- src/server/lib/envProperties-examples.js
+++ src/server/lib/envProperties-examples.js
@@ -9,9 +9,9 @@ module.exports = {
 	mysqldatabase: '[mysql-database]',
 	ACCESS_LOG: './access_log.log',
 	imagePath : './mmwrcowimg',
-	USESSL : false,
-  	SSL_CERT:  '/path/to/server_cert.pem',
-  	SSL_KEY:  '/path/to/server_key.pem',
+	USESSL : 'false',
+  	SSL_CERT:  '/path/to/server-cert.pem',
+  	SSL_KEY:  '/path/to/server-key.pem',
   	SSL_BUNDLE: '/path/to/gd_bundle-g2.crt'
 
 };
